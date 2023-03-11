4db3713d2625d13c12fb2867732bc225812b59d9
prasanthyadav <prasanthyadav549@gmail.com>
2023-03-12 00:34:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,177 @@
-
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [language, setLanguage] = useState('en');
+ // const [prevLanguage, setPrevLanguage] = useState('en');
+  const [content, setContent] = useState(['hello world', 'dev community']);
+  const [translated, setTranslated] = useState('');
+ // const [inputText, setInputText] = useState('');
+ // const [translatedText, setTranslatedText] = useState([]);
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const handleLanguageChange = (e) => {
+   // setPrevLanguage(language);
+    setLanguage(e.target.value);
+  };
+
+  // const handleInputChange = (e) => {
+  //   setInputText(e.target.value);
+  // };
+
+
+  const fetchTranslated = async () => {
+    const { data } = await axios.post(
+      'https://translation.googleapis.com/language/translate/v2',
+      {},
+      {
+        params: {
+          q: content,
+          target: language,
+          key: API_KEY,
+        },
+      }
+    );
+    setTranslated(data.data.translations[0].translatedText);
+    console.log('translatedText' ,translated)
+  };
+  
+  // const fetchTranslatedText = async () => {
+  //   const { data } = await axios.post(
+  //     'https://translation.googleapis.com/language/translate/v2',
+  //     {},
+  //     {
+  //       params: {
+  //         q: inputText,
+  //         target: language,
+  //         key: API_KEY,
+  //       },
+  //     }
+  //   );
+  //   console.log(data.data.translations[0].translatedText)
+  //   setTranslatedText(data.data.translations[0].translatedText);
+  //   console.log(inputText)
+  // };
+
+  useEffect(() => {
+    fetchTranslated();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [language]);
+
   return (
-    <div className="App">
-        <BrowserRouter>
-          <h1>hello world</h1>
-        </BrowserRouter>
+    <div>
+      <select value={language} onChange={handleLanguageChange}>
+        <option value="en">English</option>
+        <option value="es">Spanish</option>
+        <option value="fr">French</option>
+      </select>
+      <div>
+        <h1>original text</h1>
+        <h2>{translated}</h2>
+      </div>
+
+      {/* <input type="text" value={translatedText} onChange={handleInputChange} /> */}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   const [language, setLanguage] = useState('en');
+//   const [inputText, setInputText] = useState('');
+//   const [translatedText, setTranslatedText] = useState('');
+//   const API_KEY = process.env.REACT_APP_API_KEY;
+
+//   const handleLanguageChange = (e) => {
+//     setLanguage(e.target.value);
+//   };
+//   const changeToEnglish = async (content) => {
+
+//     const { data } = await axios.post(
+//       'https://translation.googleapis.com/language/translate/v2',
+//       {},
+//       {
+//         params: {
+//           q: content,
+//           target: 'en',
+//           key: API_KEY,
+//         },
+//       }
+//     );
+//     setInputText(data.data.translations[0].translatedText);
+
+//   }
+//   const translate = async()=> {
+//     const { data } = await axios.post(
+//       'https://translation.googleapis.com/language/translate/v2',
+//       {},
+//       {
+//         params: {
+//           q: inputText,
+//           target: language,
+//           key: API_KEY,
+//         },
+//       }
+//     );
+//     setTranslatedText(data.data.translations[0].translatedText);
+//   }
+
+//   const handleInputChange = async (e) => {
+//     changeToEnglish(e.target.value)
+//     translate()
+    
+//     console.log('translatedText',translatedText);
+//     console.log('originalText' ,inputText);
+//   };
+    
+
+//   useEffect(() => {
+    
+//       changeToEnglish(inputText);
+//       translate();
+    
+      
+    
+//   }, [language])
+  
+    
+//   return (
+
+//     <div>
+//     <div>
+//       <select value={language} onChange={handleLanguageChange}>
+//         <option value="en">English</option>
+//         <option value="es">Spanish</option>
+//         <option value="fr">French</option>
+//       </select>
+//       </div>
+//       <div>
+//         <h2>welcome to dev world</h2>
+//         <h2>welcome to dev world</h2>
+//         <h2>welcome to dev world</h2>
+//       </div>
+//       <input type="text" value={language==='en'?inputText: translatedText} onChange={handleInputChange} />
+//       <div>
+//         <h3>originalText: {inputText}</h3>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import i18n from 'i18next';
+// import { initReactI18next } from 'react-i18next';
+
+
+// const resources = {
+//   en: {
+//     translation: {
+//       'Welcome to my app': 'Welcome to my app',
+//       'Hello, {{name}}!': 'Hello, {{name}}!',
+//       'This is a paragraph of text.': 'This is a paragraph of text.',
+//       'Click me!': 'Click me!',
+//     },
+//   },
+//   es: {
+//     translation: {
+//       'Welcome to my app': 'Bienvenido a mi aplicación',
+//       'Hello, {{name}}!': '¡Hola, {{name}}!',
+//       'This is a paragraph of text.': 'Este es un párrafo de texto.',
+//       'Click me!': '¡Haz clic en mí!',
+//     },
+//   },
+//   fr: {
+//     translation: {
+//       'Welcome to my app': 'Bienvenue sur mon application',
+//       'Hello, {{name}}!': 'Bonjour, {{name}}!',
+//       'This is a paragraph of text.': 'Voici un paragraphe de texte.',
+//       'Click me!': 'Cliquez sur moi!',
+//     },
+//   },
+// };
+
+// i18n.use(initReactI18next).init({
+//   resources,
+//   lng: 'en',
+//   interpolation: {
+//     escapeValue: false, // react already safes from xss
+//   },
+// });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
